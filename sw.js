aacd9bbc179bcbc2c991677eca296c6ab2ee2fa9
shirleykkky <19952130866@189.cn>
2026-08-20 08:42:05 +0800

    bump cache version to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yanyan-edu-v3';
+const CACHE_NAME = 'yanyan-edu-v4';
 const ASSETS = ['./', './index.html', './data.js', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
